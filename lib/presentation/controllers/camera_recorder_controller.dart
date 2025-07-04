8cec5dec6ccf227ef275034b8faa7c371acbe369
PescadoMuerto1 <mbl0502727793@gmail.com>
2025-07-04 15:15:03 +0300

    Change the uncs to use imageStream insted of video rec

--- lib/presentation/controllers/camera_recorder_controller.dart
+++ lib/presentation/controllers/camera_recorder_controller.dart
@@ -7,21 +7,32 @@ class CameraRecorderController {
   CameraController get cameraController => _cameraController;
   bool get isRecording => _isRecording;
 
+  final List<CameraImage> _frameBuffer = [];
+
   Future<void> initializeCamera() async {
     final cameras = await availableCameras();
-    _cameraController = CameraController(cameras.first, ResolutionPreset.high);
+    _cameraController = CameraController(
+      cameras.first,
+      ResolutionPreset.high,
+      fps: 30,
+    );
     await _cameraController.initialize();
   }
 
   Future<void> startRecording() async {
     if (!_cameraController.value.isInitialized || _isRecording) return;
-    await _cameraController.startVideoRecording();
+    _frameBuffer.clear();
+    _cameraController.startImageStream((CameraImage image){
+      if (_isRecording) {
+        _frameBuffer.add(image);
+      }
+    });
     _isRecording = true;
   }
 
   Future<void> stopRecording() async {
     if (!_cameraController.value.isInitialized || !_isRecording) return;
-    await _cameraController.stopVideoRecording();
+    await _cameraController.stopImageStream();
     _isRecording = false;
   }
 
