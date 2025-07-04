914097185edf2c4ca10dd58e4e17c2817093a47a
PescadoMuerto1 <mbl0502727793@gmail.com>
2025-07-04 13:13:30 +0300

    Add home page and basic app architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:replay_it/presention/pages/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Replay It',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Replay It Home Page'),
-        ),
-        body: Center(
-          child: Text('Welcome to Replay It!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'play_shot',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
     );
   }
 }
